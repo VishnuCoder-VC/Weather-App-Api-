e1a63bc98a1204c53ea749ad078596354a9f5897
VishnuCoder-VC <vishnuraghav9286@gmail.com>
2026-06-23 13:30:49 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# import requests
+
+
+# api_key = "xxx"
+# city = input("Enter City Name: ")
+
+
+# url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+
+# response = requests.get(url)
+
+# data = response.json()
+
+
+# print(f"Temp: {data['main']['temp']}")
+# print(f"Humidity: {data['main']['humidity']}")
+# print(f"description: {data['weather'][0]['description']}")
